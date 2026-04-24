097c9eaec0e86f38795152d0dfa44281cf50adfd
nilton-f-o-junior <nilton.f.o.junior@gmail.com>
2026-04-24 17:39:51 -0300

    docs: atualização de todos os docs agora direto no nixos

--- system/nixos/nixos/home.nix
+++ system/nixos/nixos/home.nix
@@ -1,11 +1,9 @@
-# home
-
 { config, pkgs, ... }:
 
 {
-  home.username = "user";
+  home.username      = "user";
   home.homeDirectory = "/home/user";
-  home.stateVersion = "25.11";
+  home.stateVersion  = "25.11";
 
   imports = [
     ./config/helix.nix
@@ -13,40 +11,52 @@
     ./config/wezterm.nix
   ];
 
+  # niri
+  home.file.".config/niri/config.kdl".source          = ./config/niri.kdl;
+  home.file.".config/niri/wallpaper_script.sh".source = ./config/wallpaper_script.sh;
+  home.file.".config/niri/wallpaper" = {
+    source   = ./config/wallpaper;
+    recursive = true;
+  };
+
+  #
   programs.bash = {
     enable = true;
     shellAliases = {
       collegarb = "sudo nix-collect-garbage -d";
-      rebuild = "sudo nixos-rebuild switch --flake .";
+      rebuild   = "sudo nixos-rebuild switch --flake .";
     };
   };
 
+  # git
   programs.git = {
-    enable = true;
-    userName = "nilton-f-o-junior";
+    enable    = true;
+    userName  = "nilton-f-o-junior";
     userEmail = "nilton.f.o.junior@gmail.com";
 
     extraConfig = {
       init.defaultBranch = "main";
-      pull.rebase = false;
+      pull.rebase        = false;
     };
   };
 
+  # ssh
   programs.ssh = {
-    enable = true;
+    enable         = true;
     addKeysToAgent = "yes";
 
     matchBlocks = {
       "github.com" = {
-        hostname = "github.com";
-        user = "git";
+        hostname     = "github.com";
+        user         = "git";
         identityFile = "~/.ssh/id_ed25519";
       };
     };
   };
 
+  # pacotes
   home.packages = with pkgs; [
-    # applications
+    # app
     swaybg
     rofi
 
@@ -59,8 +69,7 @@
     rust-analyzer
     rustfmt
 
-    # language serve
+    # extensao
     marksman
   ];
-
 }
